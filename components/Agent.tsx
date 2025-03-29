6d3480b403d07cca774e062505b6a9f699d198d2
UjjwalFullStackDev <ujjwal.tyagi90611@gmail.com>
2025-03-29 17:57:49 +0530

    Impliment interview session

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { interviewer } from '@/constants';
 import { cn } from '@/lib/utils';
 import { vapi } from '@/lib/vapi.sdk';
 import Image from 'next/image'
@@ -19,7 +20,7 @@ interface SavedMessage {
     content: string;
 }
 
-const Agent = ({ userName, userId, type }: AgentProps) => {
+const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) => {
     const router = useRouter();
     const [isSpeaking, setIsSpeaking] = useState(false);
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
@@ -59,19 +60,61 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
         }
     }, []);
 
+    // TODO: Generate a feedback
+    const handleGenerateFeedback = async (message:SavedMessage) => {
+        console.log('Generate feedback here')
+
+        const { success, id } = {
+            success: true,
+            id: 'feedback-id'
+        }
+
+        if(success && id) {
+            router.push(`/interview/${interviewId}/feedback`)
+        } else {
+            console.log('Error saving feedback')
+            router.push('/')
+        }
+    }
+
     useEffect(() => {
-        if(callStatus === CallStatus.FINISHED) router.push('/');
+        if(callStatus === CallStatus.FINISHED) {
+            if(type === 'generate') {
+                router.push('/');
+            } else {
+                handleGenerateFeedback(messages);
+            }
+        }
+        
     },[messages, callStatus, type, userId]);
 
     const handleCall = async () => {
         setCallStatus(CallStatus.CONNECTING);
 
-        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
-            variableValues: {
-                username: userName,
-                userid: userId,
+        if(type === 'generate'){
+
+            await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+                variableValues: {
+                    username: userName,
+                    userid: userId,
+                }
+            })
+        } else {
+            let foramttedQuestions = '';
+
+            if(questions) {
+                foramttedQuestions = questions
+                .map((question) => `- ${question}`)
+                .join('\n');
             }
-        })
+
+            await vapi.start(interviewer, {
+                variableValues: {
+                    questions: foramttedQuestions
+                }
+            })
+        }
+
     }
 
     const handleDisconnect = () => {
